98c0332011967ad4d7de98c911a30f5a72e1b453
DesiZafirova <desszafirowa@gmail.com>
2025-01-24 17:13:14 +0200

    installing and importing reactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div>hello</div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
