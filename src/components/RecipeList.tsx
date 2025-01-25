7340b932e8215e532e75a3cf2222227288cf0430
DesiZafirova <desszafirowa@gmail.com>
2025-01-25 11:56:29 +0200

    styling

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -3,7 +3,7 @@ import { fetchRecipes } from '../services/api';
 import { type FetchedRecipesResponse } from '../types/recipe';
 import Spinner from './Spinner';
 import Button from './Button';
-
+import styles from './RecipeList.module.css';
 function RecipeList() {
   const { data, isLoading, isError, error } = useQuery<FetchedRecipesResponse>({
     queryKey: ['recipes'],
@@ -15,11 +15,11 @@ function RecipeList() {
   if (isError) return <div>Error: {error.message}</div>;
 
   return (
-    <ul>
+    <ul style={styles}>
       {data?.results.map((recipe) => (
-        <li key={recipe.id}>
+        <li key={recipe.id} style={styles}>
           <img src={recipe.image} alt={recipe.title} />
-          <h2>{recipe.title}</h2>
+          <h2 style={styles}>{recipe.title}</h2>
           <Button>Go to the recipe &rarr;</Button>
         </li>
       ))}
