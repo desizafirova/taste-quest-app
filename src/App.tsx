eae3639a178c2cd148cb949865e7c6b65d63b106
DesiZafirova <desszafirowa@gmail.com>
2025-01-25 12:14:22 +0200

    building routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import RecipeList from './components/RecipeList';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
+import OurMission from './pages/OurMission';
+import Recipes from './pages/Recipes';
+import Recipe from './pages/Recipe';
+import Home from './pages/Home';
 
 const queryClient = new QueryClient();
+
+const Router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/ourmission', element: <OurMission /> },
+  {
+    path: '/recipes',
+    element: <Recipes />,
+    children: [{ path: '/recipes/:id', element: <Recipe /> }],
+  },
+]);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RecipeList />
+      <RouterProvider router={Router} />
+      <Outlet />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
