3bf325a3cc3cc48190c8fdbbcc6d1bcedbef0a00
DesiZafirova <desszafirowa@gmail.com>
2025-02-09 13:00:17 +0200

    implemented lazy loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,106 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import OurMission from './pages/OurMission';
-import Recipes from './pages/Recipes';
-import Recipe from './pages/Recipe';
-import Home from './pages/Home';
 import AppLayout from './components/AppLayout';
-import Login from './pages/Login';
-import ProtectedRoute from './pages/ProtectedRoute';
-import Favourites from './pages/Favourites';
-import UserProfile from './pages/UserProfile';
-import NotFound from './pages/NotFound';
+import { lazy, Suspense } from 'react';
+import Spinner from './components/Spinner';
 
 const queryClient = new QueryClient();
+
+const Home = lazy(() => import('./pages/Home'));
+const OurMission = lazy(() => import('./pages/OurMission'));
+const Recipe = lazy(() => import('./pages/Recipe'));
+const Recipes = lazy(() => import('./pages/Recipes'));
+const Login = lazy(() => import('./pages/Login'));
+const Favourites = lazy(() => import('./pages/Favourites'));
+const UserProfile = lazy(() => import('./pages/UserProfile'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+const ProtectedRoute = lazy(() => import('./pages/ProtectedRoute'));
 
 const Router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/ourmission', element: <OurMission /> },
+      {
+        path: '/',
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <Home />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/ourmission',
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <OurMission />
+          </Suspense>
+        ),
+      },
       {
         path: '/recipes',
-        element: <Recipes />,
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <Recipes />
+          </Suspense>
+        ),
       },
-      { path: '/recipes/:id', element: <Recipe /> },
-      { path: '/login', element: <Login /> },
+      {
+        path: '/recipes/:id',
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <Recipe />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/login',
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <Login />
+          </Suspense>
+        ),
+      },
       {
         path: 'favourites',
         element: <ProtectedRoute />,
-        children: [{ path: '', element: <Favourites /> }],
+        children: [
+          {
+            path: '',
+            element: (
+              <Suspense fallback={<Spinner />}>
+                <Favourites />
+              </Suspense>
+            ),
+          },
+        ],
       },
       {
         path: '/profile',
-        element: <ProtectedRoute />,
-        children: [{ path: '', element: <UserProfile /> }],
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <ProtectedRoute />
+          </Suspense>
+        ),
+        children: [
+          {
+            path: '',
+            element: (
+              <Suspense fallback={<Spinner />}>
+                <UserProfile />
+              </Suspense>
+            ),
+          },
+        ],
       },
       {
         path: '*',
-        element: <NotFound />,
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <NotFound />
+          </Suspense>
+        ),
       },
     ],
   },
