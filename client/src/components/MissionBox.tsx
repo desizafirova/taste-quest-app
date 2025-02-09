5bcb3e37bc2204a667c55de4db0a15a3ed2d0ebc
DesiZafirova <desszafirowa@gmail.com>
2025-02-09 13:11:28 +0200

    implement lazy-load-image-component

--- client/src/components/MissionBox.tsx
+++ client/src/components/MissionBox.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import styles from './MissionBox.module.css';
 
 type MissionBoxProps = {
@@ -10,7 +12,14 @@ type MissionBoxProps = {
 function MissionBox({ img, alt, children }: MissionBoxProps) {
   return (
     <div className={styles.missionBox}>
-      <img src={img} alt={alt} className={styles.img} />
+      <LazyLoadImage
+        src={img}
+        alt={alt}
+        className={styles.img}
+        effect="blur"
+        width="100%"
+        height="100%"
+      />
       <p className={styles.p}>{children}</p>
     </div>
   );
