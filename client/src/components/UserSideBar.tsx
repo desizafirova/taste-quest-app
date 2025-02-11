f679b0331303bb81892ab3831b10c98d308a834a
DesiZafirova <desszafirowa@gmail.com>
2025-02-11 23:26:20 +0200

    fix styles

--- client/src/components/UserSideBar.tsx
+++ client/src/components/UserSideBar.tsx
@@ -5,12 +5,12 @@ function UserSideBar() {
   return (
     <nav className={styles.userSideBar}>
       <ul className={styles.ul}>
-        <li>
+        <li className={styles.li}>
           <Button textOnly={true} noBorder={true}>
             My Recipes
           </Button>
         </li>
-        <li>
+        <li className={styles.li}>
           <Button textOnly={true} noBorder={true}>
             Settings
           </Button>
